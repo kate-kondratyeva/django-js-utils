71a22e201e8b867eedb236208b4d70ffbbefe5ec
Antti Kaihola <antti@2general.com>
2012-11-17 21:36:37 +0200

    Bumped version number to 0.1-g2.1

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'django_js_utils', 
-    version='0.1-g2.1',
+    version='0.1-g2.2',
     description='Django URL Exposure to Javascript',
     author='Dimitri Gnidash',
     author_email='dimitri.gnidash@gmail.com',
